c789ed66f92434c4c674f7e0ac864bce50870d5c
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-05-21 01:50:55 +0700

    Buoi 3: SCSS

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,6 +27,31 @@ export default function HomePage(props) {
           </nav>
         </div>
       </header>
+
+      <section className="carousel">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              {/* <div>
+                <h1>CyberPhone</h1>
+                <h3>Best Smartphone of 2021!</h3>
+                <p>Cras tortor mi, lobortis quis ornare in, varius scelerisque urna. Vivamus imperdiet dolor nec odio convallis consequat</p>
+              </div> */}
+              <div>
+                <button className="btn btn-info">
+                  <i className="fa-solid fa-circle-info"></i>More info
+                </button>
+                <button className="btn btn-shop">
+                  <i className="fa-solid fa-cart-shopping"></i>Shop
+                </button>
+              </div>
+            </div>
+            {/* <div className="col-6">
+              <img src="./image/product-header2.png" alt="..." />
+            </div> */}
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
